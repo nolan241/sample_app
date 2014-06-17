4731972eda99300054d557503f45b9d9a5de9666
Nolan Morache <nolan241@gmail.com>
2014-06-17 12:54:07 -0700

    June 17th fix session helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,14 +11,14 @@ module SessionsHelper
     !current_user.nil?
   end
   
-  #def current_user
-  #  remember_token = User.digest(cookies[:remember_token])
-  #  @current_user ||= User.find_by(remember_token: remember_token)
-  #end
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
 
-  #def current_user?(user)
-  #  user == current_user
-  #end
+  def current_user?(user)
+    user == current_user
+  end
 
   #def current_user=(user)
   #  @current_user = user
