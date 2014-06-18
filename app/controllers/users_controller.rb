5152544751a459a18e688315d80b668541a303f2
Nolan Morache <nolan241@gmail.com>
2014-06-18 13:59:29 -0700

    microposts malarchy june 18

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,14 +65,15 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters
+          # Before filters
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
+              # moved to session helper
+              #def signed_in_user
+              #  unless signed_in?
+              #    store_location
+              #    redirect_to signin_url, notice: "Please sign in."
+              #  end
+              #end
 
     def correct_user
       @user = User.find(params[:id])
